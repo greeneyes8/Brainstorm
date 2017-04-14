cd9d08cc3f508bcfc5db0ab7b814fdb3c89e844a
Konrad Dobros <kd226622@gmail.com>
2017-04-15 00:22:39 +0200

    Core wireframe proposition

--- core/src/main/scala/brainstorm/core/MindMap.scala
+++ core/src/main/scala/brainstorm/core/MindMap.scala
@@ -1,1 +1,5 @@
 package brainstorm.core
+
+class MindMap (var name: String, val root: Node) {
+  def this(name: String) = this(name, new Node(name, Nothing))
+}
--- core/src/main/scala/brainstorm/core/Node.scala
+++ core/src/main/scala/brainstorm/core/Node.scala
@@ -1,1 +1,15 @@
 package brainstorm.core
+
+case class RootDeletion extends Exception {
+
+}
+
+class Node (var text: String, var parent: Option[Node]) {
+  val children = Set()
+  def remove() = {
+    parent match {
+      case Some(parent) => parent.children -= this
+      case None => throw new RootDeletionException()
+    }
+  }
+}
