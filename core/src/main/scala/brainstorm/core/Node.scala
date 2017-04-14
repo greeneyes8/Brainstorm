cee3f37445bbf78abbce440de73f5f1cf445d18e
Konrad Dobros <kd226622@gmail.com>
2017-04-15 00:35:07 +0200

    Type specification

--- core/src/main/scala/brainstorm/core/Node.scala
+++ core/src/main/scala/brainstorm/core/Node.scala
@@ -1,12 +1,12 @@
 package brainstorm.core
 
-case class RootDeletion extends Exception {
+case class RootDeletionException extends Exception {
 
 }
 
 class Node (var text: String, var parent: Option[Node]) {
-  val children = Set()
-  def remove() = {
+  val children: collection.mutable.Set[Node] = collection.mutable.Set()
+  def remove(): Unit = {
     parent match {
       case Some(parent) => parent.children -= this
       case None => throw new RootDeletionException()
