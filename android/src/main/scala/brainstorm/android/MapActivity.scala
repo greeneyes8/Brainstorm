1d1dd739ab1fbff68a4a28193906bc3e4fd1c219
Konrad Dobros <kd226622@gmail.com>
2017-05-15 14:45:51 +0200

    KMWTW

--- android/src/main/scala/brainstorm/android/MapActivity.scala
+++ android/src/main/scala/brainstorm/android/MapActivity.scala
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.content.res.Configuration
 import java.io.File
 
-class MapActivity(val file:File) extends AppCompatActivity with TypedFindView {
+class MapActivity(val file:File) extends DrawerLayoutActivity with TypedFindView {
   implicit val context = this
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
