0d52f70a364f10d6e91f1f3e1a84e545ee6fb3ad
Konrad Dobros <kd226622@gmail.com>
2017-05-17 23:16:28 +0200

    MindMapModel sketch

--- core/src/main/scala/brainstorm/core/MindMapModel.scala
+++ core/src/main/scala/brainstorm/core/MindMapModel.scala
@@ -16,6 +16,12 @@ class MindMapModel(val mindMap: MindMap) {
     }
   }
   def textChange(start: Integer, oldLines: Seq[String], newLines: Seq[String]) = {
-
+    // make change at lowest common ancestor
+    // TODO: 1. Proposition
+    //    find lowest common ancestor
+    //    and parse newlines from there (parser.parseText)
+    //    2. Proposition
+    //    go line to line and using above functions change structure 
+    //    note: propably faster the bigger the changes - one repin versus parsing whole tree anew
   }
 }
