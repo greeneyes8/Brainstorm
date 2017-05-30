073d77e61ace09af5587ac167c6ff7b12498790e
Nigurasi <nigura-asi@wp.pl>
2017-05-30 18:31:50 +0200

    tests with pending

--- android/src/test/scala/brainstorm/android/NodeAdapterTest.scala
+++ android/src/test/scala/brainstorm/android/NodeAdapterTest.scala
@@ -1,46 +1,39 @@
 package brainstorm.android
 
 class NodeAdapterTest extends CommonSpec {
-    //convertOne
-    //convertWhole
-    //node adapter convers mindmap into view
-    //it got a map and each node convers into a view 
-
-    //czy sie parenty zgadzaja
-    //czy wszystko to sa image view
-    //czy struktura jest poprawna
-    //mockContext (?)
-
     "A Node Adapter" when {
         "converting only root in a mind map" should {
             "return empty parents" in {
-                pending
+                (pending)
             }
             "got one view" in {
-
+                (pending)
+            }
+            "be such structure" in {
+                (pending)
             } 
         }
         "converting empty mind map" should {
             "return empty Seq" in {
-
+                (pending)
             } 
         }
         "converting root and two children" should {
             "return Seq with three views" in {
-
+                (pending)
             }
             "first element in Seq has no parents" in {
-
+                (pending)
             }
         }
         "wrong coords" should {
             "throw an exception" in {
-
+                (pending)
             }
         }
         "convertOne while empty seq" should {
             "throw an exception" in {
-
+                (pending)
             }
         }
         "changed root" should {
