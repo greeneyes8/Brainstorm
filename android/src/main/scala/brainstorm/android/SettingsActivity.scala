4d4d764a7d5b0642decd62d1315459728d49d091
Konrad Dobros <kd226622@gmail.com>
2017-06-25 12:09:10 +0200

    Mergered fixed

--- android/src/main/scala/brainstorm/android/SettingsActivity.scala
+++ android/src/main/scala/brainstorm/android/SettingsActivity.scala
@@ -12,19 +12,17 @@ import TypedResource._
 
 class SettingsActivity extends DrawerLayoutActivity 
   with TypedFindView with OnSharedPreferenceChangeListener {
- //   lazy val sharedPreferences : SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+  override def onCreate(savedInstanceState: Bundle) {
+    PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
+    mySharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+    setTitle(R.string.action_GeneralSettings)
+    setFragment(new SettingsFragment())
+  }
 
-    override def onCreate(savedInstanceState: Bundle) {
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
-        //afterOnCreate(savedInstanceState)
-        setTitle(R.string.action_GeneralSettings)
-        setFragment(new SettingsFragment())
-    }
-   override def onSharedPreferenceChanged(sharedPreferences: SharedPreferences,
-          key: String) {
-      recreate()
-   }
+  override def onSharedPreferenceChanged(sharedPreferences: SharedPreferences,
+    key: String) {
+      recreate
+  }
 }
